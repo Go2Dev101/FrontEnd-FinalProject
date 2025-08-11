54c6e10e3864d9f6b3d3eba2e1f0424803223086
supasin <dev.snakphu7@gmail.com>
2025-08-12 01:07:27 +0700

    แก้ App.jsx

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -1,12 +1,19 @@
+import { Badge } from "@/components/ui/badge"
 export const MenuCard = () => {
   return (
     <div className="rounded-lg max-w-80 bg-white">
-      <img src="" alt="" />
+      {/* <img src="" alt="" /> */}
       <div className="p-4">
-        <p className="text-xl font-medium text-primary-900">c</p>
-        <p className="text-sm text-secondary-400"></p>
+        <div>
+          <p className="text-xl font-medium text-primary-900 mb-1">c</p>
+          <p className="text-sm text-secondary-400"></p>
+        </div>
+
+        <div className="flex gap-2">
+          <Badge variant="default |outline | secondary | destructive">Badge</Badge>
+        </div>
+        <p className="text-end text-xl font-medium text-primary-900">200 THB</p>
       </div>
-      <div></div>
     </div>
   );
 };
