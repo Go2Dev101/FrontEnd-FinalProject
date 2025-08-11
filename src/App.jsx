a64c332bc2a3b673c2e71242e53733ce925ae5d4
supasin <dev.snakphu7@gmail.com>
2025-08-12 01:11:46 +0700

    fix import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Layout } from "./components/Layout";
 import { MenuSet } from "./views/menu/MenuSet";
+import { OrderSuccess } from "./views/checkout/OrderSuccess";
 // import { Home } from "./views/Home";
 
 
