74a51c0c0b1be9744b7217722cf54f1b53d35d87
Fahsai0526 <nathathai.pumw@gmail.com>
2025-08-11 11:33:21 +0700

    edit home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export const Home = () => {
-  return (
-    <div className='bg-blue-400'>Home</div>
-  )
-}
+  return <div className="bg-blue-400">Home555</div>;
+};
