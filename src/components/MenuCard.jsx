9094b3aaae40668c2bfaca28ac215bd51ffe01d0
Chafik-Rd <78787036+Chafik-Rd@users.noreply.github.com>
2025-08-11 17:52:32 +0700

    fix(MenuCard): add price in card

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -1,17 +1,21 @@
-import { Tag } from "./Tag"
+import { Tag } from "./Tag";
 
 export const MenuCard = () => {
   return (
     <div className="rounded-lg max-w-80 bg-white">
-        <img src="" alt=""/>
-        <div className="p-4">
-            <p className="text-xl font-medium text-primary-900">c</p>
-            <p className="text-sm text-secondary-400"></p>
-        </div>
+      <img src="" alt="" />
+      <div className="p-4">
         <div>
-            <Tag/>
-            <Tag icon={true} text={"220kcal"}/>
+          <p className="text-xl font-medium text-primary-900 mb-1">c</p>
+          <p className="text-sm text-secondary-400"></p>
         </div>
+
+        <div className="flex gap-2">
+          <Tag text={"Sh"} />
+          <Tag icon={true} text={"220kcal"} />
+        </div>
+        <p className="text-end text-xl font-medium text-primary-900">200 THB</p>
+      </div>
     </div>
-  )
-}
+  );
+};
