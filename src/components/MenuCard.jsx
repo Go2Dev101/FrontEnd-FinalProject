3008892ead14e3a623442154688af5f4b75d7e4a
titinart <k.titinart@gmail.com>
2025-08-11 17:55:45 +0700

    delete Tag.jsx component

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -1,17 +1,12 @@
-import { Tag } from "./Tag"
-
 export const MenuCard = () => {
   return (
     <div className="rounded-lg max-w-80 bg-white">
-        <img src="" alt=""/>
-        <div className="p-4">
-            <p className="text-xl font-medium text-primary-900">c</p>
-            <p className="text-sm text-secondary-400"></p>
-        </div>
-        <div>
-            <Tag/>
-            <Tag icon={true} text={"220kcal"}/>
-        </div>
+      <img src="" alt="" />
+      <div className="p-4">
+        <p className="text-xl font-medium text-primary-900">c</p>
+        <p className="text-sm text-secondary-400"></p>
+      </div>
+      <div></div>
     </div>
-  )
-}
+  );
+};
