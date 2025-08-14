3cff81614451d99dfdc686d43824585443e8c486
Chafik-Rd <78787036+Chafik-Rd@users.noreply.github.com>
2025-08-15 01:39:11 +0700

    fix(function): edit function handleCart and handleOrders

--- src/context/MessageProvider.jsx
+++ src/context/MessageProvider.jsx
@@ -1,23 +1,28 @@
 import { useState } from "react";
 import { MessageContext } from "./MessageContext";
-import { useNavigate } from "react-router-dom";
 
 export const MessageProvider = ({ children }) => {
-  const [orders, setOrders] = useState([{menuId:"",quantity:"",deliveryDate:""}]);
-  const navigate = useNavigate();
+  const [orders, setOrders] = useState([]);
 
-  const handleCart = (e) => {
-    setOrders({...orders,e});
+  const handleCart = (menuId, quantity = 0, deliveryDate) => {
+    const menu = orders.find((menu) => menu.menuId === menuId);
+    if (menu) {
+      menu.quantity =
+        quantity !== 0 ? menu.quantity + quantity : menu.quantity + 1;
+    } else {
+      setOrders([
+        ...orders,
+        { menuId: menuId, quantity: quantity, deliveryDate: deliveryDate },
+      ]);
+    }
   };
 
-  const handleOrders = (e) => {
-    setOrders({...orders,e});
-    navigate(`/`);
+  const handleOrders = (navigate,menuId, quantity = 0, deliveryDate) => {
+    handleCart(menuId, quantity , deliveryDate)
+    navigate("/ordersummary");
   };
   return (
-    <MessageContext
-      value={{ orders, handleOrders, handleCart }}
-    >
+    <MessageContext value={{ orders, handleOrders, handleCart }}>
       {children}
     </MessageContext>
   );
