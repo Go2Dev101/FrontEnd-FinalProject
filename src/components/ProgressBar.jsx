4551e2456e2ea5eca16d4e9d041992e62889b5a1
titinart <k.titinart@gmail.com>
2025-08-14 18:50:08 +0700

    create ProgressBar

---
+++ src/components/ProgressBar.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+
+export const ProgressBar = ({path}) => {
+  return (
+    <>
+      <div className="flex">
+        <Link to={`/${path}`}>
+          <p className="font-bold text-3xl text-primary-800 mr-6 ">Back</p>
+        </Link>
+        <div className="relative">
+          <div className="flex gap-4">
+            <div className="flex flex-col items-center">
+              <div className="w-11 h-11 rounded-full border-3 border-primary-900 bg-background-50 z-1"></div>
+              <p className="font-bold text-primary-800">Order Summary</p>
+            </div>
+
+            <div className="flex flex-col items-center">
+              <div className="w-11 h-11 rounded-full border-3 border-primary-900 bg-primary-900 z-1 "></div>
+              <p className="font-bold text-primary-800">Delivery Address</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <div className="w-11 h-11 rounded-full border-3 border-primary-900 bg-background-50 z-1 "></div>
+              <p className="font-bold text-primary-800 text-center">Payment</p>
+            </div>
+            <div className="w-3/4 h-1 mt-5 bg-primary-900 absolute left-9"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+
