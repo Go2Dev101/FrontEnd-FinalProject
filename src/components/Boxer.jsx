6086e3e1c33ee3e0eac1d8f56b6cd799cb7c734f
Chafik-Rd <78787036+Chafik-Rd@users.noreply.github.com>
2025-08-15 10:28:09 +0700

    fix(style): change min height

--- src/components/Boxer.jsx
+++ src/components/Boxer.jsx
@@ -1,6 +1,6 @@
 export const Boxer = ({children,className}) => {
   return (
-    <div className="min-h-[calc(100vh-224px)] flex flex-col bg-background-100 px-4 sm:px-8 py-6">
+    <div className="min-h-[calc(100vh-172px)] flex flex-col bg-background-100 px-4 sm:px-8 py-6">
       <section className={`px-4 py-6 bg-background-50 rounded-xl flex-1 ${className}`}>
         {children}
       </section>
--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,6 +1,6 @@
 export const Menu = ({children,className}) => {
   return (
-    <div className="min-h-screen flex flex-col bg-background-100 px-4 sm:px-8 py-6">
+    <div className="min-h-[calc(100vh-172px)] flex flex-col bg-background-100 px-4 sm:px-8 py-6">
       <ul className="flex gap-2 sm:gap-4">
         <li className={`px-3 sm:px-4 py-2 cursor-pointer bg-background-50 rounded-t-xl `}>Menu set</li>
         {/* <li className={`px-3 sm:px-4 py-2 cursor-pointer`}>Menu plan</li> */}
