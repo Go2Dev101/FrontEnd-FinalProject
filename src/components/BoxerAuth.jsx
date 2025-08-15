877a9e0a9a465f40b214f213e7a6f659c69f004f
Chafik-Rd <78787036+Chafik-Rd@users.noreply.github.com>
2025-08-15 10:31:02 +0700

    feat(BoxerAuth): created boxer auth component

---
+++ src/components/BoxerAuth.jsx
@@ -0,0 +1,7 @@
+export const BoxerAuth = ({children}) => {
+  return (
+    <div className="min-h-[calc(100vh-172px)] flex justify-center items-center bg-background-100 px-3 py-6">
+        {children}
+    </div>
+  )
+}
