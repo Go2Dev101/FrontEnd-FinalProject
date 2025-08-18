afa01fa31027e802d4ae30344726209d3aac3fd2
Chafik-Rd <78787036+Chafik-Rd@users.noreply.github.com>
2025-08-18 23:18:14 +0700

    fix(infoBar): change font size

--- src/components/menu/InfoBar.jsx
+++ src/components/menu/InfoBar.jsx
@@ -1,29 +1,29 @@
 export const InfoBar = ({ onClick, menuBar }) => {
   return (
-    <div className="max-w-125 w-full border border-primary-900 flex justify-between text-xs font-light mx-auto mb-3">
+    <div className="max-w-125 w-full border border-primary-900 flex justify-between text-sm sm:text-base mx-auto mb-3">
       <div
         onClick={() => onClick("info")}
-        className={`h-10 w-full flex justify-center items-center capitalize cursor-pointer ${
+        className={`h-10 w-full flex justify-center items-center capitalize cursor-pointer text-center ${
           menuBar === "info" && "bg-primary-900 text-white font-medium"
         }`}
       >
-        <p className="font-light">information</p>
+        <p className="line-clamp-1">information</p>
       </div>
       <div
         onClick={() => onClick("ingre")}
-        className={`h-10 w-full flex justify-center items-center capitalize cursor-pointer ${
+        className={`h-10 w-full flex justify-center items-center capitalize cursor-pointer text-center ${
           menuBar === "ingre" && "bg-primary-900 text-white font-medium"
         }`}
       >
-        <p>ingredients</p>
+        <p className="line-clamp-1">ingredients</p>
       </div>
       <div
         onClick={() => onClick("nutri")}
-        className={`h-10 w-full flex justify-center items-center capitalize cursor-pointer ${
+        className={`h-10 w-full flex justify-center items-center capitalize cursor-pointer text-center ${
           menuBar === "nutri" && "bg-primary-900 text-white font-medium"
         }`}
       >
-        <p>nutrition facts</p>
+        <p className="line-clamp-1">nutrition facts</p>
       </div>
     </div>
   );
