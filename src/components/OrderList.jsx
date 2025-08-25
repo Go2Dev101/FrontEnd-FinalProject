bf830f4ecab8078b4460dcb6818eff899e236c42
Fahsai0526 <nathathai.pumw@gmail.com>
2025-08-25 09:23:11 +0700

    edit: OrderList add icon

--- src/components/OrderList.jsx
+++ src/components/OrderList.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { Card } from "./ui/card";
 import { OrderCounter } from "./OrderCounter";
+import { Trash2 } from "lucide-react";
 
 export const OrderList = ({ data }) => {
   console.log(data);
   return (
     <>
-      <div className="flex flex-col w-full">
-        <Card className="max-w-200 w-full p-4 min-h-186">
-          <div id="OrderItems" className="">
-            <div className="flex">
+      <div className="">
+        <Card className="w-full p-4 min-h-186">
+          <div id="OrderItems" className="flex justify-around items-center">
+            <div className="flex gap-10 items-center">
               <img
                 src="./public/img/Set Menu IMG.svg"
                 className="max-w-40 w-full"
@@ -22,8 +23,11 @@ export const OrderList = ({ data }) => {
                 <p>Delivery Date: 5-12 July</p>
                 <p className="text-2xl">{data.priceTHB} THB</p>
               </div>
-              <div id="Count">
+              <div>
                 <OrderCounter />
+              </div>
+              <div>
+                <Trash2 />
               </div>
             </div>
           </div>
