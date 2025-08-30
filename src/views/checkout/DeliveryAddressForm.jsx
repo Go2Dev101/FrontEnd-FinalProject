b14e6ff7a9c252cbe63bd103040b059f2869cc1d
titinart <k.titinart@gmail.com>
2025-08-30 14:35:09 +0700

    feat(add): import Card from shadcn /X from lucide

--- src/views/checkout/DeliveryAddressForm.jsx
+++ src/views/checkout/DeliveryAddressForm.jsx
@@ -6,9 +6,28 @@ import { CheckOutHeader } from "../../components/CheckOutHeader";
 import { Button } from "@/components/ui/button";
 import { OrderTotal } from "../../components/OrderTotal";
 import { useState } from "react";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { X } from 'lucide-react';
 
+const pickUpPoints = [
+  "Chula Hospital",
+  "Udom Suksa School",
+  "The Tree Condo",
+  "MRT Samyan",
+  "The One Building",
+];
 export const DeliveryAddressForm = () => {
-  const [edit, setEdit] = useState(false);
+  const [edit, setEdit] = useState(true);
+  const [deliveryAddress, setDeliveryAddress] = useState("");
 
   return (
     <>
@@ -40,9 +59,7 @@ export const DeliveryAddressForm = () => {
                   <p>Name</p>
                   <p>Tel :091-234-5678 </p>
                 </div>
-                <p className="text-xl text-primary-700">
-                  51/233 M.5 T.Bangyai A.Bangyai Nonthaburi
-                </p>
+                <p className="text-xl text-primary-700">{deliveryAddress}</p>
 
                 <SquarePen
                   onClick={() => setEdit(true)}
@@ -54,27 +71,16 @@ export const DeliveryAddressForm = () => {
                 <div className="flex flex-col justify-between gap-6 text-2xl font-bold text-primary-700">
                   <p>Select 1 Pickup Point</p>
                   <div className="flex gap-3 flex-wrap sm:">
-                    <Button className="max-w-75 w-full bg-white text-primary-700 hover:bg-gray-100/80">
-                      Chula Hospital
-                    </Button>
-                    <Button className="max-w-75 w-full bg-white text-primary-700 hover:bg-gray-100/80">
-                      Udom Suksa School
-                    </Button>
-                    <Button className="max-w-75 w-full bg-white text-primary-700 hover:bg-gray-100/80">
-                      The Tree Condo
-                    </Button>
-                    <Button className="max-w-75 w-full bg-white text-primary-700 hover:bg-gray-100/80">
-                      MRT Samyan
-                    </Button>
-                    <Button
-                      onClick={() => setEdit(true)}
-                      className="max-w-75 w-full bg-white text-primary-700 hover:bg-gray-100/80"
-                    >
-                      The One Building
-                    </Button>
+                    {pickUpPoints.map((point, index) => (
+                      <Button
+                        key={index}
+                        className="max-w-75 w-full bg-white text-primary-700 hover:bg-gray-100/80"
+                      >
+                        {point}
+                      </Button>
+                    ))}
                   </div>
                 </div>
-                
               </div>
             </div>
           </div>
@@ -84,7 +90,87 @@ export const DeliveryAddressForm = () => {
       {/* pop-up address */}
       {edit && (
         <div className="h-screen w-full fixed top-0 left-0 bg-black/80 z-60 overscroll-none flex justify-center items-center">
-          <OrderTotal />
+          {/* <div className="max-w-124 w-full bg-white flex flex-col mb-3 p-6 rounded-xl shadow-md">
+            <label>House Number</label>
+            <textarea
+              name=""
+              id=""
+              className="bg-red-300 text-green-200"
+            ></textarea>
+            <label>Subdistrict</label>
+            <textarea
+              name=""
+              id=""
+              className="bg-red-300 text-green-200"
+            ></textarea>
+            <label>Kwange</label>
+            <textarea
+              name=""
+              id=""
+              className="bg-red-300 text-green-200"
+            ></textarea>
+            <label>Zip code</label>
+            <textarea
+              name=""
+              id=""
+              className="bg-red-300 text-green-200"
+            ></textarea> */}
+          <Card className="w-full max-w-sm p-3">
+            <CardHeader>
+              <CardTitle className="text-2xl">
+                Edit Delivery Address
+                </CardTitle>
+              {/* <CardDescription>
+                Enter your email below to login to your account
+              </CardDescription> */}
+              <CardAction>
+                <X onClick={() =>  setEdit(false) }
+                  className="cursor-pointer"/>
+              </CardAction>
+            </CardHeader>
+            <CardContent>
+              <form>
+                <div className="flex flex-col gap-6">
+                  <div className="grid gap-2">
+                    <label htmlFor="email">Street address</label>
+                    <Input
+                      
+                      type="address"
+                      placeholder="House Number and street name"
+                      required
+                    />
+                  </div>
+                  <div className="grid gap-2">
+                    <div className="flex items-center">
+                      <label htmlFor="password">Sub-District</label>
+                    </div>
+                    <Input id="password" type="password" required />
+                  </div>
+                  <div className="grid gap-2">
+                    <div className="flex items-center">
+                      <label htmlFor="password">District</label>
+                    </div>
+                    <Input id="password" type="password" required />
+                  </div>
+                  <div className="grid gap-2">
+                    <div className="flex items-center">
+                      <label htmlFor="password">Postcode</label>
+                    </div>
+                    <Input id="password" type="numeric" required />
+                  </div>
+                </div>
+              </form>
+            </CardContent>
+            <CardFooter className="flex-col gap-2">
+              <Button type="submit" className="w-full">
+                Save
+              </Button>
+              <Button variant="outline" className="w-full">
+                Back
+              </Button>
+            </CardFooter>
+          </Card>
+          {/* </div> */}
         </div>
       )}
     </>
