aed299a17633db3bab55c4e2ed7ff50e08cdc97a
Chafik-Rd <78787036+Chafik-Rd@users.noreply.github.com>
2025-09-07 00:04:05 +0700

    fix(views): edit signup(edit firstName),menuSet(edit sort map data) and menuSetDetail(add loading)

--- src/views/menu/MenuSet.jsx
+++ src/views/menu/MenuSet.jsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
 import { Menu } from "../../components/menu/Menu";
 import { MenuCard } from "../../components/MenuCard";
-// import { menuSet } from "../../data/menuSet";
-import { getAllMenus } from "../../services/menuService";
+import { getAllMenus } from "../../services/menuService.js";
 
 export const MenuSet = () => {
   const [menus, setMenus] = useState([]);
@@ -21,19 +20,11 @@ export const MenuSet = () => {
   return (
     <>
       <Menu className="flex flex-row gap-6 flex-wrap justify-center md:justify-start">
-        {menus.map((menu) => (
-          <MenuCard
-            key={menu._id}
-            name={menu.title}
-            detail={menu.description}
-            img={menu.imageUrl}
-            category={menu.category}
-            price={menu.price}
-            kcal={menu.kcal}
-            menuId={menu._id}
-            slug={menu.slug}
-          />
-        ))}
+        {menus
+          .sort((a, b) => a.durationDays - b.durationDays)
+          .map((menu) => (
+            <MenuCard key={menu._id} data={menu} />
+          ))}
       </Menu>
     </>
   );
--- src/views/menu/MenuSetDetail.jsx
+++ src/views/menu/MenuSetDetail.jsx
@@ -4,25 +4,25 @@ import { MenuDetail } from "../../components/menu/MenuDetail";
 import { useEffect, useState } from "react";
 import { getMenuBySlug } from "../../services/menuService.js";
 
+
 export const MenuSetDetail = () => {
   const { menuSlug } = useParams();
   const [menu, setMenu] = useState("");
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchMenu = async () => {
+      setLoading(true);
       try {
         const respon = await getMenuBySlug(menuSlug);
         setMenu(respon.menu);
+        setLoading(false);
       } catch (err) {
         console.error(err);
       }
     };
     fetchMenu();
   }, [menuSlug]);
-  // const menu = menuSet.find((menu) => menu.id === menuSlug);
-  return (
-    <>
-      <MenuDetail path={"menuset"} menu={menu} />
-    </>
-  );
+
+  return <MenuDetail path={"menuset"} menu={menu} loading={loading} />;
 };
