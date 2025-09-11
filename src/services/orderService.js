00a08061b513489254a808ebf0c5d1bc29876bca
Chafik-Rd <78787036+Chafik-Rd@users.noreply.github.com>
2025-09-11 23:17:48 +0700

    fix(Payment): add feat update status order

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -4,10 +4,16 @@ import api from "./api.js";
 export const createOrder = async () => {
   const response = await api.post("/api/order");
   return response.data;
-}
+};
 
 // Get all orders for the logged-in user role admin
 export const getAllOrders = async () => {
   const response = await api.get("/api/order");
   return response.data;
-}
+};
+
+// Update order status
+export const updateOrderStatus = async (orderId, status) => {
+  const response = await api.put(`/api/order/${orderId}`, { status });
+  return response.data;
+};
