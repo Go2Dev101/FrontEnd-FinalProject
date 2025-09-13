2d6a952be52a73a92c43d88873cdfd7dd471c993
supasin <dev.snakphu7@gmail.com>
2025-09-13 21:08:28 +0700

    edit(api):add withCredentials: true  ever api way

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,18 +2,24 @@ import api from "./api.js";
 
 // Login user
 export const loginUser = async (userData) => {
-  const response = await api.post("/api/user/login", userData);
+  const response = await api.post("/api/user/login", userData, {
+    withCredentials: true,
+  });
   return response.data;
 };
 
 // Logout user
 export const logoutUser = async () => {
-  const response = await api.post("/api/user/logout");
+  const response = await api.post("/api/user/logout", {
+    withCredentials: true,
+  });
   return response.data;
 };
 
 // Signup user
 export const signupUser = async (userData) => {
-  const response = await api.post("/api/user/signup", userData);
+  const response = await api.post("/api/user/signup", userData, {
+    withCredentials: true,
+  });
   return response.data;
 };
--- src/services/menuService.js
+++ src/services/menuService.js
@@ -2,12 +2,14 @@ import api from "./api.js";
 
 // Get all menus
 export const getAllMenus = async () => {
-  const response = await api.get("/api/menu");
+  const response = await api.get("/api/menu", { withCredentials: true });
   return response.data;
 };
 
 // Get menu by slug
 export const getMenuBySlug = async (slug) => {
-  const response = await api.get(`/api/menu/${slug}`);
+  const response = await api.get(`/api/menu/${slug}`, {
+    withCredentials: true,
+  });
   return response.data;
 };
