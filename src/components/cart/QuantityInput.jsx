1fb0682b58634d906cc44e38d7403ec23d4007c7
Fahsai0526 <nathathai.pumw@gmail.com>
2025-09-13 15:32:03 +0700

    fix(OrderSummary): fix responsvie

--- src/components/cart/QuantityInput.jsx
+++ src/components/cart/QuantityInput.jsx
@@ -6,12 +6,12 @@ export const QuantityInput = ({ setCount, count, onChange }) => {
     onChange?.(newValue);
   };
   return (
-    <div className="p-2 rounded-full border flex w-31 justify-between items-center bg-white">
+    <div className="p-2 rounded-full border flex w-20 justify-between items-center bg-white md:w-31">
       <span
         onClick={() => handleChange(count - 1)}
         className={`rounded-full flex justify-center items-center cursor-pointer bg-secondary-200
         ${
-          count === 1 && "pointer-events-none opacity-80 w-6 h-6 md:w-8 md:h-8"
+          count === 1 && "pointer-events-none opacity-80 w-4 h-4 md:w-6 md:h-6"
         }`}
       >
         <Minus />
@@ -19,7 +19,7 @@ export const QuantityInput = ({ setCount, count, onChange }) => {
       <p>{count}</p>
       <span
         onClick={() => handleChange(count + 1)}
-        className="rounded-full flex justify-center items-center cursor-pointer bg-secondary-200 w-6 h-6 md:w-8 md:h-8"
+        className="rounded-full flex justify-center items-center cursor-pointer bg-secondary-200 w-4 h-4 md:w-6 md:h-6"
       >
         <Plus />
       </span>
